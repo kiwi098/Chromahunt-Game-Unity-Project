fcb85efb96fd5a57ca965e64e17a508f2c116897
Keneth <cayaskeneth13@gmail.comgit config --global user.name Keneth>
2022-09-21 14:12:26 +0800

    Added Player Movement Script & Made Player Prefab

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private LayerMask groundLayer;
+    private Rigidbody2D body;
+    private BoxCollider2D boxcoll;
+    private float JumpCooldown;
+    private float horizontalinput;
+
+    private void Awake()
+    {
+        //Grab references for rigidbody and animator from object
+        body = GetComponent<Rigidbody2D>();
+        boxcoll = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        horizontalinput = Input.GetAxis("Horizontal");
+        
+
+        //Flip Player to left and right
+        if (horizontalinput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalinput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        //Jump Logic
+        if (JumpCooldown > 0.2f)
+        {
+            body.velocity = new Vector2(horizontalinput * speed, body.velocity.y);
+            if (isGrounded() && Input.GetKey(KeyCode.Space))
+            {
+                body.gravityScale = 7;
+                Jump();
+            }
+        }
+        else
+            JumpCooldown += Time.deltaTime;
+    }
+    private void Jump()
+    {
+        if (isGrounded())
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpPower);
+        }
+        else
+            JumpCooldown = 0;
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxcoll.bounds.center, boxcoll.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+}
+

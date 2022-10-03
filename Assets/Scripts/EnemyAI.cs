b6793252404729e24eca3eb9c4b501ae33f461de
kiwi098 <83574695+kiwi098@users.noreply.github.com>
2022-10-03 15:01:27 +0800

    Chase Player Enemy AI

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    public float Health = 100;
-    public float MoveSpeed = 10;
+    public float Health;
+    public float MoveSpeed;
+    public float AttackRange;
+    public float JumpPower;
+    public LayerMask groundLayer;
 
-    public GameObject[] Players;
+    private GameObject[] Players;
+    private GameObject ClosestPlayer;
 
     private Rigidbody2D Body;
     private BoxCollider2D BoxCollider;
@@ -15,17 +19,92 @@ public class EnemyAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Players = GameObject.FindGameObjectsWithTag("Player");
+        ClosestPlayer = null;
+
+        Body = GetComponent<Rigidbody2D>();
+        BoxCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        EnemyMove();
+        Players = GameObject.FindGameObjectsWithTag("Player");
+
+        for (int ctr = 0; ctr < Players.Length; ctr++)
+        {
+            if (ClosestPlayer == null)
+            {
+                ClosestPlayer = Players[ctr];
+            }
+            else if (Vector2.Distance(Players[ctr].transform.position, transform.position) < 
+                Vector2.Distance(ClosestPlayer.transform.position, transform.position))
+            {
+                ClosestPlayer = Players[ctr];
+            }
+        }
+
+        transform.eulerAngles = Vector3.zero;
+        ChasePlayer();
+        AttackPlayer();
+        Death();
     }
 
-    void EnemyMove()
+    void ChasePlayer()
     {
+        float InputX = 0;
+        float InputY = Body.velocity.y;
 
+        if (AttackRange < Vector2.Distance(ClosestPlayer.transform.position, transform.position))
+        {
+            if (Vector2.Distance(new Vector2(ClosestPlayer.transform.position.x, 0), new Vector2(transform.position.x, 0)) >=
+                Vector2.Distance(new Vector2(0, ClosestPlayer.transform.position.y), new Vector2(0, transform.position.y)))
+            {
+                if ((ClosestPlayer.transform.position - transform.position).x < 0)
+                {
+                    transform.localScale = new Vector3(-1, 1, 1);
+                    InputX = -1 * MoveSpeed;
+                }
+                else
+                {
+                    transform.localScale = Vector3.one;
+                    InputX = 1 * MoveSpeed;
+                }
+            }
+            else
+            {
+                if (isGrounded())
+                {
+                    if (ClosestPlayer.transform.position.y > transform.position.y)
+                    {
+                        Body.gravityScale = 7;
+                        InputY = 1 * JumpPower;
+                    }
+                }
+            }
+        }
+
+        Body.velocity = new Vector2(InputX, InputY);
+    }
+
+    void AttackPlayer()
+    {
+        if (AttackRange < Vector2.Distance(ClosestPlayer.transform.position, transform.position))
+        {
+
+        }
+    }
+
+    bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(BoxCollider.bounds.center, BoxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+
+    void Death()
+    {
+        if (Health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }

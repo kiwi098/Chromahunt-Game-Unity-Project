bede47b359fc7e54d5ee4bed3908a4a111941c79
kiwi098 <83574695+kiwi098@users.noreply.github.com>
2022-10-10 21:18:16 +0800

    More Debug

--- Assets/Scripts/UI Scripts/SpawnPlayers.cs
+++ Assets/Scripts/UI Scripts/SpawnPlayers.cs
@@ -2,22 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Cinemachine;
 
 public class SpawnPlayers : MonoBehaviour
 {
+    public GameObject CM_Vcam;
+
     public GameObject[] playerPrefabs;
     public Transform[] spawnPoints;
-
- 
 
     private void Start()
     {
 
          int randomNumber = Random.Range(0, spawnPoints.Length);
          Transform spawnPoint = spawnPoints[randomNumber];
-         //GameObject playerToSpawn = playerPrefabs[(int)PhotonNetwork.LocalPlayer.CustomProperties["playerAvatar"]];
+         GameObject playerToSpawn = playerPrefabs[(int)PhotonNetwork.LocalPlayer.CustomProperties["playerAvatar"]];
          Debug.Log(PhotonNetwork.LocalPlayer.CustomProperties["playerAvatar"]);
-         PhotonNetwork.Instantiate(playerPrefabs[(int)PhotonNetwork.LocalPlayer.CustomProperties["playerAvatar"]].name, spawnPoint.position, Quaternion.identity);
+         GameObject SpawnedPlayer = PhotonNetwork.Instantiate(playerToSpawn.name, spawnPoint.position, Quaternion.identity);
+        CM_Vcam.GetComponent<CinemachineVirtualCamera>().Follow = SpawnedPlayer.transform;
      }
     }
 
